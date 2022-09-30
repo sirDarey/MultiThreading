8dc1b2036d90f5f3e0e4cc39a0208cca478058e6
Sir Darey <darey1oguns@gmail.com>
2022-09-30 02:24:30 +0100

    Creating a thread by implementing the Runnable Interface

--- _2_MyCounter.java
+++ _2_MyCounter.java
@@ -9,7 +9,7 @@ import java.util.logging.Logger;
  * 
  * Creating a Thread By Implementing the Runnable Interface
  */
-public class _2_MyCounter extends Thread{
+public class _2_MyCounter implements Runnable{
     int threadNo;
     
     public _2_MyCounter(int threadNo) {
@@ -34,21 +34,12 @@ public class _2_MyCounter extends Thread{
         }            
     }
     
-    public static void main(String[] args) throws InterruptedException {
-        _2_MyCounter count1 = new _2_MyCounter(1);
-        _2_MyCounter count2 = new _2_MyCounter(2);
-        
-        
-        Long startTime = System.currentTimeMillis();
-        count1.start();
-        System.out.println("***************");
-        count2.start();
-        
-        sleep(2505);
-        
-        Long endTime = System.currentTimeMillis();
-        System.out.println("Total time taken: "+ (endTime-startTime));
-        System.out.println();
+    public static void main(String[] args){
+        Thread thread1 = new Thread(new _2_MyCounter(1));
+        Thread thread2 = new Thread(new _2_MyCounter(2));
+       
+        thread1.start();
+        thread2.start();
     }
     
 }
